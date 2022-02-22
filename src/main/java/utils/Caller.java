7f0a17d5af08b9393edf53536ae6244d5a8bc877
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-02-22 13:40:18 +0100

    Update Caller.java

--- src/main/java/utils/Caller.java
+++ src/main/java/utils/Caller.java
@@ -1,9 +1,7 @@
 package utils;
 
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
