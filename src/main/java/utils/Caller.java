46993b3eb1e28af6e1b31574c62c5ca7fb8ad528
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-02-22 15:39:59 +0100

    Added new exception type

--- src/main/java/utils/Caller.java
+++ src/main/java/utils/Caller.java
@@ -1,14 +1,15 @@
 package utils;
 
-import org.junit.Assert;
+import exceptions.FailedCallException;
 import retrofit2.Call;
 import retrofit2.Response;
 import java.io.IOException;
 
 public class Caller {
+
     static Printer log = new Printer(Caller.class);
 
-    protected static <T> T perform(Call<T> call, Boolean strict, String serviceName){
+    protected static <T> T perform(Call<T> call, Boolean strict, String serviceName) throws FailedCallException {
         try {
             Response<T> response = call.execute();
 
@@ -21,14 +22,13 @@ public class Caller {
                 log.new Warning(response.message());
                 log.new Warning("The response code is: " + response.code());
                 if (strict)
-                    Assert.fail("The strict call performed for " + serviceName + " service returned response code " + response.code());
+                    throw new FailedCallException("The strict call performed for " + serviceName + " service returned response code " + response.code());
+                return null;
             }
         }
         catch (IOException e) {
             log.new Error(e.getStackTrace());
-            Assert.fail("The call performed for " + serviceName + " failed for an unknown reason.");
+            throw new FailedCallException("The call performed for " + serviceName + " failed for an unknown reason.");
         }
-        return null;
     }
-
 }
