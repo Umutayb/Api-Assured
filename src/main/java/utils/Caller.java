6be2af270051bd3ea00d7b4314a74a34ad93b19b
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-03-12 13:23:17 +0100

    Added new caller method

--- src/main/java/utils/Caller.java
+++ src/main/java/utils/Caller.java
@@ -9,7 +9,7 @@ public class Caller {
 
     static Printer log = new Printer(Caller.class);
 
-    protected static <T> T perform(Call<T> call, Boolean strict, Boolean body, String serviceName) {
+    protected static <T> T perform(Call<T> call, Boolean strict, String serviceName) {
         log.new Info("Performing an api call to " + call.request().url());
         try {
             Response<T> response = call.execute();
@@ -18,10 +18,7 @@ public class Caller {
                 if (response.message().length()>0)
                     log.new Info(response.message());
                 log.new Success("The response code is: " + response.code());
-                if (body)
-                    return (T) response;
-                else
-                    return response.body();
+                return response.body();
             }
             else{
                 if (response.message().length()>0)
@@ -33,6 +30,33 @@ public class Caller {
         }
         catch (IOException e) {
             log.new Error(e.getStackTrace());
+            Assert.fail("The call performed for " + serviceName + " failed for an unknown reason.");
+        }
+        return null;
+    }
+
+    protected static <T> Response<T> getResponse(Call<T> call, Boolean strict, String serviceName) {
+        log.new Info("Performing an api call to " + call.request().url());
+        try {
+            Response<T> response = call.execute();
+
+            if (response.isSuccessful()){
+                if (response.message().length()>0)
+                    log.new Info(response.message());
+                log.new Success("The response code is: " + response.code());
+                return response;
+            }
+            else{
+                if (response.message().length()>0)
+                    log.new Warning(response.message());
+                log.new Warning("The response code is: " + response.code());
+                if (strict)
+                    Assert.fail("The strict call performed for " + serviceName + " service returned response code " + response.code());
+            }
+        }
+        catch (IOException e) {
+            log.new Error(e.getStackTrace());
+            log.new Error(e.getCause());
             Assert.fail("The call performed for " + serviceName + " failed for an unknown reason.");
         }
         return null;
